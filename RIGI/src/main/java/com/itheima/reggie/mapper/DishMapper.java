6b404c2e9ea664cd31a9dcb8758b35de5d4d3b25
playboy113 <13625087940@163.com>
2022-11-20 19:37:03 +0800

    Revert "submit"
    This reverts commit 4c925ce4f76bbc64597081526293f4efad03ef97.

--- RIGI/src/main/java/com/itheima/reggie/mapper/DishMapper.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.reggie.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.itheima.reggie.domain.Dish;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DishMapper extends BaseMapper<Dish> {
-}
--- RIGI/src/main/java/com/itheima/reggie/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.reggie.domain.Employee;
-
-public interface EmployeeService extends IService<Employee> {
-}
